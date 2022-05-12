75386fa5dee8244c1eb39cb91f42f91a54cc6ade
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-05-12 10:45:50 +0200

    rest, spread, args, object then destruct

--- beginInJs/script.js
+++ beginInJs/script.js
@@ -134,35 +134,332 @@ undefined, null, boolean, string, symbol{mutable}, number and object
 //     return "large";
 //   }
 // }
-
 // console.log(testSize(20));
 // Golf Code
-var names = [
-  "Hole-in-one",
-  "Eagle",
-  "Birdie",
-  "Par",
-  "Bogey",
-  "DoubleBogey",
-  "Go Home",
-];
-function GolfScore(par, strokes) {
-  if (strokes == 1) {
-    return names[0];
-  } else if (strokes <= par - 2) {
-    return names[1];
-  } else if (strokes == par - 1) {
-    return names[2];
-  } else if (strokes == par) {
-    return names[3];
-  } else if (strokes == par + 1) {
-    return names[4];
-  } else if (strokes == par + 2) {
-    return names[5];
-  } else if (strokes == par + 3) {
-    return names[6];
-  }
+/*
+// var names = [
+//   "Hole-in-one",
+//   "Eagle",
+//   "Birdie",
+//   "Par",
+//   "Bogey",
+//   "DoubleBogey",
+//   "Go Home",
+// ];
+// function GolfScore(par, strokes) {
+//   if (strokes == 1) {
+//     return names[0];
+//   } else if (strokes <= par - 2) {
+//     return names[1];
+//   } else if (strokes == par - 1) {
+//     return names[2];
+//   } else if (strokes == par) {
+//     return names[3];
+//   } else if (strokes == par + 1) {
+//     return names[4];
+//   } else if (strokes == par + 2) {
+//     return names[5];
+//   } else if (strokes == par + 3) {
+//     return names[6];
+//   }
 
-  return "Change Me";
+//   return "Change Me";
+// }
+// console.log(GolfScore(5, 8));
+// Objects
+// var ourDog = {
+//   name: "Camper",
+//   legs: 4,
+//   tails: 1,
+//   friends: ["EveryThing!"],
+// };
+// // console.log(ourDog);
+
+// console.log(myDog.tails); // value
+// console.log(myDog["name"]); // key
+// var legsNum = 4;
+// console.log(myDog[legsNum]);
+// var myDog = {
+//   name: "fox",
+//   legs: 4,
+//   tails: 1,
+//   friends: ["father", "mother"],
+// };
+// // add a new prop:-
+// myDog["bark"] = "woof!";
+// console.log(myDog);
+// // updating class
+// myDog.name = "foxa";
+// // delete a prop
+// delete myDog.legs;
+// console.log(myDog);
+
+// function getSelectedValFromKey(key) {
+//   var result = "";
+//   var data = { 1: "ali", 2: "aliaa", 3: "saed" };
+//   result = data[key];
+//   return result;
+// }
+// console.log(getSelectedValFromKey(2));
+// var myObj = {
+//   gift: "pony",
+//   pet: "kitten",
+//   bed: "sleigh",
+// };
+// function checkObj(myProp) {
+//   if (myObj.hasOwnProperty(myProp)) {
+//     return myObj[myProp];
+//   } else {
+//     return "Not Found";
+//   }
+// }
+
+// console.log(checkObj("gift"));
+
+// console.log(checkObj("hello"));
+*/
+/* var myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+var secondTree = myPlants[1].list[1];
+console.log(secondTree); */
+/* var collection = {
+  123456: {
+    album: "Slippery when wet",
+    artist: "Slippery when wet",
+    tracks: ["Let It Rock", "You Give love a bad name"],
+  },
+  789456: {
+    album: "1998",
+    artist: "Slippery when wet",
+    tracks: ["1990", "little and cover"],
+  },
+  123456: {
+    album: "Slippery when wet",
+    artist: "Slippery when wet",
+    tracks: ["Let It Rock", "You Give love a bad name"],
+  },
+  789456: {
+    album: "1998",
+    artist: "Slippery when wet",
+    tracks: ["Let It Rock", "You Give love a bad name"],
+  },
+};
+var collection = JSON.parse(JSON.stringify(collection));
+function updateRecords(id, prop, value) {
+  if (value === "") {
+    delete collection[id][prop];
+  } else if (prop === "tracks") {
+    collection[id][prop] = collection[id][prop] || [];
+    collection[id][prop].push(value);
+  } else {
+    collection[id][prop] = value;
+  }
+  return collection;
 }
-console.log(GolfScore(5, 8));
+
+console.log(updateRecords(123456, "artist", "ABBA")); */
+
+/* var myArr = [];
+var i = 0;
+while (i < 5) {
+  myArr.push(i);
+  i++;
+}
+console.log(myArr); */
+/* var myArr = [];
+
+for (var i = 0; i < 5; i++) {
+  myArr.push(i);
+}
+
+console.log(myArr); */
+/* var arr = [2, 3, 4, 5];
+var sum = 0;
+for (var i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+console.log(sum); */
+/* var arr = [
+  [1, 3],
+  [5, 4],
+  [4, 6, 8],
+];
+function multiplyAll(arr) {
+  var product = 1;
+  for (var r = 1; r < arr.length; r++) {
+    for (var c = 1; c < arr[r].length; c++) {
+      product *= arr[r][c];
+    }
+  }
+  return product;
+}
+
+console.log(multiplyAll(arr)); */
+/* var data = [
+  { fname: "ali", lname: "mahgoub", age: 19, likes: [2, 4, 5] },
+  { fname: "aliaa", lname: "ali", age: 20 },
+];
+function lookupProfile(name, prop) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].fname === name) {
+      return data[i][prop] || "No Such data";
+    }
+  }
+  return "No Such Content";
+}
+
+var dataGet = lookupProfile("ali", "lname");
+console.log(dataGet); */
+/* function convertToInteger(str) {
+  return parseInt(str);
+}
+var converted = convertToInteger("90");
+console.log(converted); */
+
+/* function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+var converted = convertToInteger("10011");
+console.log(converted); */
+
+// conditional ternary
+
+/* function checkEqual(a, b) {
+  return a === b ? "true" : "false";
+}
+
+console.log(checkEqual(2, 3)); */
+
+/* function checkSign(num) {
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+}
+console.log(checkSign(-2)); */
+
+/* var a = 0;
+a = 9;
+console.log(a);
+
+let name1 = "ali";
+name1 = "aliaa";
+console.log(name1); */
+
+// var and let in function and block scope
+/* function checkScope() {
+  "use strict";
+  // block scope
+  let i = "fn scope";
+  if (true) {
+    let i = "Block Scope!";
+    console.log(`block scope ${i} `);
+  }
+  console.log(` fn scope ${i} `);
+  return i;
+}
+checkScope(); */
+
+/* const pi = 3.14;
+console.log(pi);
+pi = 90;
+console.log(pi); */
+/* 
+const s = [3, 4, 5, 6];
+function editInPlace() {
+  "use strict";
+  s[0] = 9;
+  return s;
+}
+
+editInPlace();
+console.log(s); */
+
+// anonyms fn ==> hasn't a name
+/* var a = function () {
+    return " anonyms fn ==> hasn't a name ";
+};
+console.log(a());
+ */
+
+// var magic = () => {
+//   return new Date();
+// };
+
+/* var magic = () => new Date();
+console.log(magic()); */
+
+/* var magic = (name) => {
+  return `hi there from arrow fn y ${name}`;
+};
+console.log(magic("ali")); */
+
+/* const realNum = [-9, -8, -6, 2, 0, 3, 4];
+const squareList = (arr) => {
+  const posNums = arr
+    .filter((ele) => Number.isInteger(ele) && ele > 0)
+    .map((x) => x * x)
+    .filter((ele) => ele >= 9);
+  return posNums;
+};
+const squareListNum = squareList(realNum);
+console.log(squareListNum);
+ */
+/* const increment = (function () {
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 6));
+console.log(increment(5)); */
+// rest op ==>
+/* const sum = (function () {
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3));
+
+const sumQ = function (...args) {
+  return args.reduce((a, b) => a + b, 0);
+};
+console.log(sumQ(1, 2, 3)); */
+/* const sum = (function () {
+  return function sum(...args) { // using rest here 
+    // const args = [x, y, z];
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3, 4)); */
+// spread operator to evaluate arrays in-place
+/* const arr1 = ["JAN", "FEB", "MAR", "APR", "APR", "MAY"];
+let arr2;
+(function () {
+  arr2 = [...arr1];
+  arr1[0] = "potato";
+})();
+console.log(arr2); */
+// Use destructing assignment to assign variables from objects
+/* var vole = { x: 3.6, y: 7.4, z: 6.54 };
+var x = vole.x; // =>3.6
+var y = vole.y; // =>7.4
+var z = vole.z; // =>6.54
+// destruct them:-
+const { x: a, y: b, z: c } = vole;
+console.log(`${a},${b},${c}`); */
+
+/* const AVG_Temp = {
+  today: 77.5,
+  tomorrow: 79,
+};
+function getTemp(avg_temp) {
+  "use strict";
+  const { tomorrow: tom } = avg_temp;
+  return tom;
+}
+console.log(getTemp(AVG_Temp)); */
